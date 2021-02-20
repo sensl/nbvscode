ddae00ebb18bf0635a6cfc13180da5a9af40b832
savfod <savsmail@gmail.com>
2021-02-21 02:19:57 +0300

    Deprecated param removed

--- nbvscode.py
+++ nbvscode.py
@@ -4,7 +4,7 @@ def setup_nbvscode():
     os.makedirs("/home/jovyan/vscode_projects", exist_ok=True)
     return {
         "command": ["code-server", "--port", "{port}",
-                    "--auth", "none", "--disable-ssh", "--disable-telemetry",
+                    "--auth", "none", "--disable-telemetry",
                     "--extensions-dir", "/home/jovyan/.vscode_extensions/",
                     "--user-data-dir", "/home/jovyan/.vscode_data/",
                     "/home/jovyan/vscode_projects"],

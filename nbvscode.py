2fec7d0b03d5b1514f025dae53faac7f1e228dd9
Hantao Cui <cuihantao@users.noreply.github.com>
2021-08-27 21:52:44 -0500

    Update nbvscode.py

--- nbvscode.py
+++ nbvscode.py
@@ -1,15 +1,15 @@
 import os
 
 def setup_nbvscode():
-    os.makedirs("/home/jovyan/vscode_projects", exist_ok=True)
+    os.makedirs("/home/jovyan/repos", exist_ok=True)
     return {
         "command": ["code-server", "--port", "{port}",
                     "--auth", "none", "--disable-telemetry",
                     "--extensions-dir", "/home/jovyan/.vscode_extensions/",
                     "--user-data-dir", "/home/jovyan/.vscode_data/",
-                    "/home/jovyan/vscode_projects"],
+                    "/home/jovyan/repos"],
         "absolute_url": False,
         "launcher_entry": {
-            "title": "VS Code",
+            "title": "Code Server",
         }
     }
